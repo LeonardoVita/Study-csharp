002dca3132fd0e4740ff9be76821beffb8fae38d
Leonardo Vita Milanez de Almeida <l.milanez.de.almeida@avanade.com>
2021-05-12 14:03:25 -0300

    calculating the lowest and highest grades in method showStatistics

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,12 +11,27 @@ namespace GradeBook
         public Book(string name){
             grades = new List<double>();
             this.name = name;
-            Console.WriteLine($"Construtctor This {this.name}");
         }
 
         public void addGrade(double grade){
-            grades.Add(grade);   
-            Console.WriteLine($"addGrade This {this.name}");
+            grades.Add(grade);
+        }
+
+        public void showStatistic(){          
+
+            double result = 0;
+            double highGrade = double.MinValue; 
+            double lowGrade = double.MaxValue;
+
+            foreach (var item in grades)
+            {
+                highGrade = Math.Max(item,highGrade);
+                lowGrade = Math.Min(item,lowGrade);
+                result += item;    
+            }
+            
+            result /= grades.Count;
+            Console.WriteLine($"The average is {result:n1}\nthe highgrade is {highGrade:n1}\nthe low is {lowGrade:n1}");
         }
     }
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,21 +8,14 @@ namespace GradeBook
         static void Main(string[] args)
         {   
             var book = new Book("Leonardo");
-            book.addGrade(99.1);
+            book.addGrade(12.7);
+            book.addGrade(10.3);
+            book.addGrade(9.6);
+            book.addGrade(4.1);
+            book.addGrade(7.7);
 
-            List<double> grades = new List<double>() {12.7, 10.3, 9.6, 4.1};
-
-            grades.Add(50);
-            grades[0]= 10.1;
-
-            double result = 0.0;
-            foreach (var item in grades)
-            {
-                result += item;    
-            }
+            book.showStatistic();
             
-            result /= grades.Count;
-            Console.WriteLine($"The average is {result:n1}");
              
         }
     }

a8469d017ebca7c75fabbc9d7a55ef3ea83501c6
Leonardo Vita Milanez de Almeida <l.milanez.de.almeida@avanade.com>
2021-05-20 19:07:38 -0300

    delegate can point to a method that have the same signature

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -3,8 +3,26 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
+    public delegate string WriteLogDelegate(string logMessage);
+
     public class TypeTests
     {   
+        [Fact]
+        public void WriteLogDelegateCanPointToMethod()
+        {
+            WriteLogDelegate log;
+
+            log = new WriteLogDelegate(ReturnMessage);    
+
+            var result = log("testeDelegate");
+            Assert.Equal("testeDelegate",result);
+        }
+
+        string ReturnMessage(string message)
+        {
+            return message;
+        }
+
         [Fact]
         public void ValueTypeAlsoPassByValue()
         {
